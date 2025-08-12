b3b32bcb2b61567c7852fb1c476227efb4639619
Danimouraela <danidanimoura2@gmail.com>
2025-08-12 00:12:05 +0000

    Final

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,7 +4,7 @@ int main() {
     // Carta 1 - Informações
     char estado1;
     char codigo1[4];
-    char nomeCidade1[50];
+    char nomecidade1[50];
     int populacao1;
     float area1;
     float pib1;
@@ -13,7 +13,7 @@ int main() {
     // Carta 2 - Informações
     char estado2;
     char codigo2[4];
-    char nomeCidade2[50];
+    char nomecidade2[50];
     int populacao2;
     float area2;
     float pib2;
@@ -28,7 +28,7 @@ int main() {
     scanf(" %s", codigo1);
 
     printf("Nome da Cidade: ");
-    scanf(" %[^\n]", nomeCidade1);
+    scanf(" %[^\n]", nomecidade1);
 
     printf("População: ");
     scanf("%d", &populacao1);
@@ -53,7 +53,7 @@ int main() {
     scanf(" %s", codigo2);
 
     printf("Nome da Cidade: ");
-    scanf(" %[^\n]", nomeCidade2);
+    scanf(" %[^\n]", nomecidade2);
 
     printf("População: ");
     scanf("%d", &populacao2);
@@ -73,7 +73,7 @@ int main() {
     printf("\nCarta 1:\n");
     printf("Estado: %c\n", estado1);
     printf("Código: %s\n", codigo1);
-    printf("Nome da Cidade: %s\n", nomeCidade1);
+    printf("Nome da Cidade: %s\n", nomecidade1);
     printf("População: %d\n", populacao1);
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
@@ -82,7 +82,7 @@ int main() {
     printf("\nCarta 2:\n");
     printf("Estado: %c\n", estado2);
     printf("Código: %s\n", codigo2);
-    printf("Nome da Cidade: %s\n", nomeCidade2);
+    printf("Nome da Cidade: %s\n", nomecidade2);
     printf("População: %d\n", populacao2);
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
